b918e37b49fe50ec6eb71806936c7ed0a0c82ed5
Anders Schütt <aasc.skp@edu.mercantec.dk>
2021-01-08 08:59:51 +0100

    Add project files

---
+++ H4Portfolio/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace H4Portfolio
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            var webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://anders.nntq.net"
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
+        }
+    }
+}
